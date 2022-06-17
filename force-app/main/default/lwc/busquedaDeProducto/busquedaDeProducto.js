6ffe8315f8fccbf4ca5c28bc933215aaf8a8682b
LaloRGDO <95454688+LaloRGDO@users.noreply.github.com>
2022-06-17 01:49:42 -0500

    terminando visualforce page

---
+++ force-app/main/default/lwc/busquedaDeProducto/busquedaDeProducto.js
@@ -0,0 +1,5 @@
+import { LightningElement, api, wire, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+//import searchProduct from '@salesforce/apex/LWCQuoteController.searchProduct';
+
+export default class BusquedaDeProducto extends LightningElement {}
